a01271986c5a51080f639258493d63abde951ca2
Mateus <mateus_vinicius15@outlook.com>
2020-06-23 00:58:53 -0400

    :ghost: Added component history

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import Header from './components/Header/Header';
-import History from './components/History/History';
-import Routes from './routes';
+import Header from "./components/Header/Header";
+import History from "./components/History/History";
+import Routes from "./routes";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <History />
       <Routes />
     </BrowserRouter>
   );
--- frontend/src/pages/Feed/Feed.js
+++ frontend/src/pages/Feed/Feed.js
@@ -4,6 +4,8 @@ import io from 'socket.io-client';
 
 import './Feed.css';
 
+import History from '../../components/History/History';
+
 import more from '../../assets/more.svg';
 import like from '../../assets/like.svg';
 import comment from '../../assets/comment.svg';
@@ -44,43 +46,46 @@ class Feed extends Component {
 
   render() {
     return (
-      <section className="post-list">
-        { this.state.feed.map(post => (
-          <article key={post._id}>
-            <header>
-              <div className="user">
-                <img src={`http://localhost:3333/files/${post.image}`} />
-                <div className="user-info">
-                  <span>{post.author}</span>
-                  <span className="place">{post.place}</span>
+      <>
+        <History />
+        <section className="post-list">
+          { this.state.feed.map(post => (
+            <article key={post._id}>
+              <header>
+                <div className="user">
+                  <img src={`http://localhost:3333/files/${post.image}`} />
+                  <div className="user-info">
+                    <span>{post.author}</span>
+                    <span className="place">{post.place}</span>
+                  </div>
                 </div>
-              </div>
-              
-              <img src={more} alt="Mais" />
-            </header>
-
-            <img src={`http://localhost:3333/files/${post.image}`} />
-
-            <footer>
-              <div className="actions">
-                <button type="button" onClick={() => 
-                this.handleLike(post._id)}>
-                  <img src={like} />
-                </button>
                 
-                <img src={comment} />
-                <img src={send} />
-              </div>
+                <img src={more} alt="Mais" />
+              </header>
 
-              <strong>{post.likes} curtidas</strong>
-              <p>
-                {post.description}
-                <span>{post.hashtags}</span>
-              </p>
-            </footer>
-          </article>
-        ))}
-      </section>
+              <img src={`http://localhost:3333/files/${post.image}`} />
+
+              <footer>
+                <div className="actions">
+                  <button type="button" onClick={() => 
+                  this.handleLike(post._id)}>
+                    <img src={like} />
+                  </button>
+                  
+                  <img src={comment} />
+                  <img src={send} />
+                </div>
+
+                <strong>{post.likes} curtidas</strong>
+                <p>
+                  {post.description}
+                  <span>{post.hashtags}</span>
+                </p>
+              </footer>
+            </article>
+          ))}
+        </section>
+      </>
     );
   }
 }
